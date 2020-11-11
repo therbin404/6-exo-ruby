8d651f8b48a384d76b3356b52d93dfea93d2d446
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-11-11 13:14:15 +0100

    remove view in method; return name/nickname formatted instead

--- exo_3.rb
+++ exo_3.rb
@@ -1,12 +1,13 @@
 def hello_user(name = '', nickname = '') #if name and nickname aren't not valued, set them to empty string
   if name != '' && nickname == '' #If name is valued but not nickname
-    puts "Bonjour #{name.downcase}"
+    return name.downcase # we want to return name downcased
   elsif nickname != '' && name == '' #else if nickname is valued but not name
-    puts "Bonjour #{nickname.downcase}"
+    return nickname.downcase #we want to return the nickname downcased
   else 
-    puts "Bonjour #{name.capitalize}"
+    return name.capitalize #we want to return the name with the first letter in capital letter
   end
 end
 
-puts "Entrez un nom (première ligne) et/ou un surnom (deuxième ligne):"
-hello_user(gets.chomp, gets.chomp) #put the two gets directly in the function, to avoid the declaration of 2 variables
+puts 'Entrez un nom (première ligne) et/ou un surnom (deuxième ligne):'
+names = hello_user(gets.chomp, gets.chomp) #chomp to remove the carriage return
+puts "Bonjour #{names}"
