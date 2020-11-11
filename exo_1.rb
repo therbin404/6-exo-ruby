ae69a2c63625b5f2560f899e7245ff73dfee37fa
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-11-11 13:05:35 +0100

    modified the method to return a boolean

--- exo_1.rb
+++ exo_1.rb
@@ -1,10 +1,16 @@
 def palindrome(string) 
   if string.downcase == string.reverse.downcase #If the string (downcased, to compare all words, event if the word have capital letters) is the same as the string reversed
-    puts "Le mot \"#{string}\" est un palindrome, la preuve : #{string.reverse}"
+    return true
   else 
-    puts "Le mot \"#{string}\" n'est pas un palindrome, la preuve : #{string.reverse}"
+    return false
   end
 end
 
 puts 'Entrez un mot, et voyez si c\'est un palindrome:'
-palindrome(gets.chomp) #We directly put the gets in the function. By that way, we declare one less variable
+word_to_check = gets.chomp
+
+if palindrome(word_to_check) #if the method returned true
+  puts "Le mot \"#{word_to_check}\" est un palindrome, la preuve : #{word_to_check.reverse}"
+else 
+  puts "Le mot \"#{word_to_check}\" n'est pas un palindrome, la preuve : #{word_to_check.reverse}"
+end
